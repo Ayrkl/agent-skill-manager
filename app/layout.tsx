08aaf725f5f5367071cfed16ba0d5b226b315d96
Ayrkl <cansinyrkl@gmail.com>
2026-04-03 10:53:28 +0300

    feat: initialize project structure with layout, landing page, and navigation header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,14 +29,8 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      {/* body'ye h-full ve flex-col ekledik */}
       <body className="h-full flex flex-col">
-        <div className="navbar bg-base-100 shadow-sm">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
-        </div>
-
-        {/* main'e flex-1 vererek tüm boşluğu kaplamasını sağladık. 
-            Buraya bg-base-200 gibi bir renk eklersen tüm boşluğun o renk olduğunu göreceksin. */}
+        <Header />
         <main className="flex-1 bg-base-0">{children}</main>
       </body>
     </html>
