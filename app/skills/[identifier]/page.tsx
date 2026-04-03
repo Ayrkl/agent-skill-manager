7918ae7f413a44dc54c1a8e9f730ff554635c7fd
Ayrkl <cansinyrkl@gmail.com>
2026-04-03 13:51:34 +0300

    feat: implement skills directory and detail view pages with mock data structure

--- app/skills/[identifier]/page.tsx
+++ app/skills/[identifier]/page.tsx
@@ -1,17 +1,27 @@
-import { SKILLS } from "../SKILLS";
+"use client";
 
-type SkillPageProps = {
-  params: Promise<{ identifier: string }>;
-};
-export default async function SkillPage({ params }: SkillPageProps) {
-  const { identifier } = await params;
-  const skill = SKILLS.find((skill) => skill.id === identifier);
+import { useParams } from "next/navigation";
+import { Skill, SKILLS } from "../SKILLS";
+import { useEffect, useState } from "react";
+
+export default function SkillPage() {
+  const params = useParams<{ identifier: string }>();
+  const [skills, setSkills] = useState(SKILLS);
+  const [skill, setSkill] = useState<Skill | null>(null);
+
+  useEffect(() => {
+    const foundSkill = skills.find((s) => s.id === params.identifier);
+    if (foundSkill) {
+      setSkill(foundSkill);
+    }
+  }, [params.identifier, skills]);
+
   return skill ? (
     <article>
-      <h1>{skill?.name}</h1>
-      <p>{skill?.description} </p>
-      <p>{skill?.category}</p>
-      <p>{skill?.experience}</p>
+      <h1>{skill.name}</h1>
+      <p>{skill.description}</p>
+      <p>{skill.category}</p>
+      <p>{skill.experience}</p>
     </article>
   ) : (
     <div>Skill not found</div>
--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+import { SKILLS } from "./SKILLS";
+
 export default function SkillsPage() {
+  const skills = SKILLS;
   return (
-    <div>
-      <h1>Skills</h1>
-    </div>
+    <section>
+      <ul>
+        {skills.map((skill) => (
+          <li key={skill.id}>
+            {skill.name}
+            <Link href={`/skills/${skill.id}`}>{skill.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 }
